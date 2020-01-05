2b203b4eb9b1c26698adb0c6e2e0db3acf0a5b56
liyepai <liyejiano@qq.com>
2020-01-06 00:35:22 +0800

    修改密码框的显示效果与点击有效效果

--- src/components/danyuange.vue
+++ src/components/danyuange.vue
@@ -1,15 +1,21 @@
 <template>
 <!-- 这个页面是单元格的封装 -->
-  <div class="cell">
+  <div class="cell" @click="dianji">
    <div class="left">{{zuo}}</div>
-   <div class="right">{{you}} <span class="iconfont iconjiantou1"></span></div>
+   <div class="right">{{type==='password'?'******':you}}<span class="iconfont iconjiantou1"></span></div>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['zuo','you']
-
+    props: ['zuo','you','type'],
+   
+   //修改点击
+   methods: {
+       dianji(event){
+          this.$emit('click',event) 
+       }
+   }
 }
 </script>
 

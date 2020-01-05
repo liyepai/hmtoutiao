625df42f712bfedef6ef1c75e856be7819d46a90
liyepai <liyejiano@qq.com>
2020-01-05 13:25:26 +0800

    设置拦截器
    设置请求拦截器，每次请求前 先拦截  获取本地token值  设置给请求报文中的请求头中名为Authorization的键，因为后台规定  请求都需要Authorization的认证，如果不在拦截器中设置，那么 以后每次请求都需要设置一次 请求头  那样很麻烦

--- src/utils/myaxios.js
+++ src/utils/myaxios.js
@@ -4,4 +4,39 @@ import axios from 'axios'
 
 axios.defaults.baseURL='http://localhost:3000'
 
+
+//设置拦截器  每次请求前 设置Authorization认证
+// 添加请求拦截器:所有请求都会经过这个拦截器
+axios.interceptors.request.use(function (config) {
+    // 相当于请求报文  config
+    //先获取本地token值
+    let token =localStorage.getItem('token')
+    if(token){
+    //    headers:{'Authorization':localStorage.getItem('token')}
+        config.headers.Authorization = token
+        // config（请求报文）的请求头的Authorization键的值为 token
+    }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    // console.log(response)//打印出来的是获取成功或失败的一切数据
+    if(response.data.message==='用户信息验证失败'){
+     //验证失败可能是没有token值 那么就就调回登录页面
+    //  this.$router.push({name:'Login'}) 用这个跳转不了
+     window.location.href='#/login'
+    //  http://localhost:8080/ #/login  
+    }
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+})
+
+
+
 export default axios

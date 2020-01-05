a61b63fcbb24e21d6c21fa5969cc879218962f8d
liyepai <liyejiano@qq.com>
2020-01-05 12:41:12 +0800

    封装了个人中心下面的单个单元格

---
+++ src/components/danyuange.vue
@@ -0,0 +1,27 @@
+<template>
+<!-- 这个页面是单元格的封装 -->
+  <div class="cell">
+   <div class="left">{{zuo}}</div>
+   <div class="right">{{you}} <span class="iconfont iconjiantou1"></span></div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['zuo','you']
+
+}
+</script>
+
+<style lang='less' scoped>
+.cell{
+    padding: 20px 10px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    > .right{
+        font-size: 13px;
+        color: #888;
+    }
+}
+</style>

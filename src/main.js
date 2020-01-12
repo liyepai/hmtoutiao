7a43ddefd7e2af004770ed7611706e88b1e4b76c
liyepai <liyejiano@qq.com>
2020-01-12 09:28:48 +0800

    加了一个组件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router/index'
 //引入全局样式
 import '@/style/reset.css'
-import { Toast,Uploader,Dialog,Field,Picker, Icon,Tab, Tabs,List,PullRefresh } from 'vant';
+import { Toast,Uploader,Dialog,Field,Picker, Icon,Tab, Tabs,List,PullRefresh,Search } from 'vant';
 Vue.use(Toast) //轻提示
    .use(Uploader)//文件上传的组件
    .use(Dialog )
@@ -15,7 +15,7 @@ Vue.use(Toast) //轻提示
    .use(Tabs)
    .use(List)
    .use(PullRefresh)
-
+   .use(Search)
 Vue.config.productionTip = false
 
 new Vue({
